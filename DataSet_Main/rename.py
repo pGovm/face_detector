17f0b3dc337e6629d7adc21e770fb10d52a51339
nchan18 <nchan18@outook.com>
2024-03-01 05:29:51 -0500

    renamed image tag in annotations

--- DataSet_Main/rename.py
+++ DataSet_Main/rename.py
@@ -1,18 +1,25 @@
 import os
 import re
+import json
 
 # specify the directory you want to change the files in
-directory = '/home/nitin/objsmallpp/DataSet_Main/validation/images'
+directory = '/home/nitin/objsmallpp/DataSet_Main/validation/annotations/'
 
 for filename in os.listdir(directory):
-    if filename.endswith(".jpg"):
+    # print(filename)
+    if filename.endswith(".txt"):
         # find the part in parentheses
-        match = re.search(r'\((.*?)\)', filename)
-        if match:
-            # get the number in parentheses
-            number = match.group(1)
-            # create the new filename
-            new_filename = filename.replace(f' ({number})', f'_{number}')
-            print(f'Old: {filename}, New: {new_filename}')
-            # rename the file
-            os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
+        data = json.load(open(filename))
+        data["image"] = data["image"][11:]
+        print(data)
+        with open(directory+filename, 'w') as outfile:
+            json.dump(data, outfile)
+        # match = re.search(r'\((.*?)\)', filename)
+        # if match:
+        #     # get the number in parentheses
+        #     number = match.group(1)
+        #     # create the new filename
+        #     new_filename = filename.replace(f' ({number})', f'_{number}')
+        #     print(f'Old: {filename}, New: {new_filename}')
+        #     # rename the file
+        #     os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
