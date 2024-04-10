88c1e5d7b1388cb74e8272c3b4f339df8227762a
Your Name <you@example.com>
2024-04-10 01:28:37 -0400

    Govind did it

--- DataSet_Main/Ye_Rec.py
+++ DataSet_Main/Ye_Rec.py
@@ -1,7 +1,7 @@
 from imageai.Detection.Custom import DetectionModelTrainer
 
 trainer = DetectionModelTrainer()
-data_directory="/workspace/objsmallpp/DataSet_Main"
+data_directory="C:/Users/17048/objsmallpp/DataSet_Main"
 trainer.setModelTypeAsYOLOv3()
 trainer.setDataDirectory(data_directory)
 trainer.setTrainConfig(object_names_array=["Ye"], batch_size=4, num_experiments=349, train_from_pretrained_model="/workspace/objsmallpp/DataSet_Main/models/tiny-yolov3.pt")
